1f863ff2db94ef6cfddb3a4952f77f9e86d39122
jeeeee <jeeee@msn.com>
2018-01-27 08:58:43 +0900

    課題：スライドショーアプリ

--- app/src/main/java/com/techacademy/takeda/ryoji/autoslideshowapp/MainActivity.java
+++ app/src/main/java/com/techacademy/takeda/ryoji/autoslideshowapp/MainActivity.java
@@ -7,11 +7,20 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Handler;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /*
 スライドショーアプリ
@@ -37,18 +46,51 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Timer mTimer;
+    Handler mHandler = new Handler();
+    Cursor cursor;
     private static final int PERMISSIONS_REQUEST_CODE = 100;
+    ImageView imageView;
+    ImageButton nextButton;
+    ImageButton previousButton;
+    ImageButton playButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imageView = findViewById(R.id.imageView);
+        nextButton = findViewById(R.id.nextButton);
+        previousButton = findViewById(R.id.previousButton);
+        playButton = findViewById(R.id.playButton);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_CODE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getContentsInfo();
+                } else {
+                    finish();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
         // Android 6.0以降の場合
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             // パーミッションの許可状態を確認する
-            if (checkSelfPermission(
-                    Manifest.permission.READ_EXTERNAL_STORAGE) ==
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
                     PackageManager.PERMISSION_GRANTED) {
                 // 許可されている
                 getContentsInfo();
@@ -57,18 +99,28 @@ public class MainActivity extends AppCompatActivity {
                 requestPermissions(new String[]{
                         Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSIONS_REQUEST_CODE);
             }
-            // Android 5系以下の場合
         } else {
             getContentsInfo();
         }
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (!(mTimer == null)) {
+            mTimer.cancel();
+            mTimer = null;
+        }
+        if (!(cursor == null)) {
+            cursor.close();
+        }
     }
 
     private void getContentsInfo() {
-
         // 画像の情報を取得する
         ContentResolver resolver = getContentResolver();
-        Cursor cursor = resolver.query(
+        cursor = resolver.query(
                 MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
                 null, // 項目(null = 全項目)
                 null, // フィルタ条件(null = フィルタなし)
@@ -76,20 +128,79 @@ public class MainActivity extends AppCompatActivity {
                 null // ソート (null ソートなし)
         );
 
-        if (cursor.moveToFirst()) {
-            do {
-                // indexからIDを取得し、そのIDから画像のURIを取得する
-                int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
-                Long id = cursor.getLong(fieldIndex);
-                Uri imageUri = ContentUris.withAppendedId(
-                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
-
-                ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
-                imageVIew.setImageURI(imageUri);
-            } while (cursor.moveToNext());
+        if (!cursor.moveToFirst()) {
+            Toast.makeText(this, "画像がありません。", Toast.LENGTH_LONG).show();
+            nextButton.setEnabled(false);
+            previousButton.setEnabled(false);
+            playButton.setEnabled(false);
         }
-        cursor.close();
+    }
 
+    private void getContents() {
+        // indexからIDを取得し、そのIDから画像のURIを取得する
+        int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+        Long id = cursor.getLong(fieldIndex);
+        Uri imageUri = ContentUris.withAppendedId(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+        imageView.setImageURI(imageUri);
+    }
+
+    private void moveToNext() {
+        if (!cursor.moveToNext()) {
+            cursor.moveToFirst();
+        }
+    }
+
+    private void moveToPrevious() {
+        if (!cursor.moveToPrevious()) {
+            cursor.moveToLast();
+        }
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.nextButton:
+                getContents();
+                moveToNext();
+                break;
+            case R.id.previousButton:
+                getContents();
+                moveToPrevious();
+                break;
+            case R.id.playButton:
+                if (mTimer == null) {
+                    nextButton.setEnabled(false);
+                    previousButton.setEnabled(false);
+                    playButton.setImageResource(android.R.drawable.ic_media_pause);
+                    playTask();
+                } else {
+                    nextButton.setEnabled(true);
+                    previousButton.setEnabled(true);
+                    playButton.setImageResource(android.R.drawable.ic_media_play);
+                    mTimer.cancel();
+                    mTimer = null;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void playTask() {
+        // タイマーの始動
+        mTimer = new Timer();
+        mTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        getContents();
+                        moveToNext();
+                    }
+                });
+            }
+        }, 100, 2000);    // 最初に始動させるまで 100ミリ秒、ループの間隔を 2000ミリ秒 に設定
     }
 }
 
